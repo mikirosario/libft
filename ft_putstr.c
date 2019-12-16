cf17e0e741afae457c2f5d547dba3246f0181872
Michael lucas Rosario <mrosario@c3r3s2.42madrid.com>
2019-12-16 01:12:18 +0100

    Have ft_putstr return printed bytes.

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,18 +6,22 @@
 /*   By: mrosario <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 11:53:36 by mrosario          #+#    #+#             */
-/*   Updated: 2019/12/12 12:02:37 by mrosario         ###   ########.fr       */
+/*   Updated: 2019/12/16 01:10:30 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putstr(char const *str, size_t len)
+size_t	ft_putstr(char const *str, size_t len)
 {
+	size_t	i;
+
+	i = 0;
 	if (str)
 	{
-		while (len && len-- && *str)
-			write(1, str++, 1);
+		while (len && len-- && str[i])
+			write(1, &str[i++], 1);
 	}
+	return (i);
 }
