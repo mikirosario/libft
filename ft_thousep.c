89025c2d59038405455a7f734a386c436c20be83
Michael lucas Rosario <mrosario@c3r8s2.42madrid.com>
2019-12-21 01:14:51 +0100

    Fixed ft_thousep

--- ft_thousep.c
+++ ft_thousep.c
@@ -6,37 +6,51 @@
 /*   By: mrosario <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/20 19:42:54 by mrosario          #+#    #+#             */
-/*   Updated: 2019/12/20 23:48:55 by mrosario         ###   ########.fr       */
+/*   Updated: 2019/12/21 01:13:11 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_thousep(char *numstr, char sep)
+static int	ft_sepcount(char *numstr, int strlen)
+{
+	int		comcount;
+	int		newlen;
+
+	comcount = 0;
+	if (numstr[0] != '-' && strlen > 3)
+		comcount = 1 + ((strlen - 4) / 3);
+	else if (numstr[0] == '-' && strlen > 4)
+		comcount = 1 + ((strlen - 4 / 3));
+	newlen = comcount + strlen;
+	return (newlen);
+}
+
+char		*ft_thousep(char *numstr, char sep)
 {
 	int		strlen;
-	int		comcount;
 	int		comi;
 	int		newlen;
 	char	*sepnum;
 
-	comcount = 0;
 	comi = 0;
 	if (!numstr || (!(ft_isprint(sep))))
 		return (NULL);
 	strlen = ft_strlen(numstr);
-	if (strlen > 3)
-		comcount = 1 + ((strlen - 4) / 3);
-	newlen = comcount + strlen--;
+	newlen = ft_sepcount(numstr, strlen--);
 	if (!(sepnum = ft_calloc(1 + newlen--, sizeof(char))))
 		return (NULL);
-	while (strlen >= 0 && numstr[strlen] != '-')
+	if (numstr[0] == '-')
+	{
+		sepnum[0] = numstr[0];
+		strlen--;
+		numstr++;
+	}
+	while (strlen >= 0)
 	{
 		comi == 3 ? (sepnum[newlen--] = sep) : \
 					(sepnum[newlen--] = numstr[strlen--]);
 		comi == 3 ? comi -= 3 : comi++;
 	}
-	if (numstr[strlen] == '-')
-		sepnum[newlen] = numstr[strlen];
 	return (sepnum);
 }
