cabbb8c434093feeab99c68bacdffc0d8347b26e
Michael lucas Rosario <mrosario@c1r14s3.42madrid.com>
2020-07-07 18:03:38 +0200

    Fixed mistyped variable name. :p

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mrosario <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 22:48:05 by mrosario          #+#    #+#             */
-/*   Updated: 2020/07/07 17:59:12 by mrosario         ###   ########.fr       */
+/*   Updated: 2020/07/07 18:02:31 by mrosario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_list	*ft_lstnew(void const *content)
 	{
 		tmp->content = (void *)content;
 		tmp->next = NULL;
-		tmp->len = ft_strlen(tmp);
+		tmp->len = ft_strlen(content);
 	}
 	return (tmp);
 }
