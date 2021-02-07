22e58081f7efadaec358a5f2f66f7b49dc4e5997
Miki Rosario <chisparroja@gmail.com>
2021-02-08 00:55:43 +0100

    Fixed bug that caused list members to be freed before the address of the next list member could be retrieved, which could cause segfaults

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrosario <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: miki <miki@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 14:35:33 by mrosario          #+#    #+#             */
-/*   Updated: 2019/11/16 15:06:46 by mrosario         ###   ########.fr       */
+/*   Updated: 2021/02/08 00:53:50 by miki             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	del(lst->content);
-	free(lst);
-	lst = NULL;
+	if (del)
+	{
+		del(lst->content);
+		free(lst);
+	}
 }
