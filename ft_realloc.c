df0e648a80750f0a6401524152c9b381efa54cfa
Miki Rosario <chisparroja@gmail.com>
2021-03-26 02:51:28 +0100

    Protected ft_realloc against new_size == 0 or new_size < old_size

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: miki <miki@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 22:09:22 by miki              #+#    #+#             */
-/*   Updated: 2021/03/26 02:07:20 by miki             ###   ########.fr       */
+/*   Updated: 2021/03/26 02:50:14 by miki             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 ** new memory block of the size defined by new_size, freeing the old memory
 ** block.
 **
-** If a null pointer is passed, a null pointer will be returned and nothing
-** will be freed.
+** If a null pointer is passed, or if new_size is 0, or if new_size is less than
+** old_size, a null pointer will be returned and the old memory will not be
+** freed.
 **
 ** If the memory allocation fails, a null pointer will be returned and the old
-** memory will not be destroyed
+** memory will not be freed.
 **
 ** If old_size is 0, this function will just act the same as ft_calloc and
 ** reserve new_size bytes of memory.
@@ -29,9 +30,6 @@
 ** If old_size > 0, then this function will use memcpy, which is not safe if dst
 ** and src overlap, to copy the data from ptr to the corresponding bytes of the
 ** new memory. The old memory will then be freed.
-**
-** Behaviour is undefined if size is less than the memory to be reallocated.
-** Probably means segfault. So just don't do that. ;)
 **
 ** Remember the size of an array is not the same as the number of elements in
 ** it. An array of four integers is 4 * sizeof(int) = 16 bytes. You need to
@@ -46,7 +44,7 @@ void	*ft_realloc(void *ptr, size_t new_size, size_t old_size)
 {
 	void	*tmp;
 
-	if (!ptr)
+	if (!ptr || !new_size || new_size < old_size)
 		return (NULL);
 	ptr = ft_calloc(new_size, 1);
 	if (!ptr)
